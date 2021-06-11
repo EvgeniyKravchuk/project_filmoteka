7cf04913d31a39b6265d7a11cd7cb7f39b9d2866
Jakkanda <51709348+Jakkanda@users.noreply.github.com>
2021-06-11 15:39:21 +0300

    Changes basic function of rendering

--- src/js/components/server-answers/render-page-func.js
+++ src/js/components/server-answers/render-page-func.js
@@ -4,6 +4,11 @@ const refs = {
   queryError: document.querySelector('.error_text'),
 };
 export default function renderPage(body) {
-  const markup = MovieCardTemplate(body.results);
+    const modifiedArr = body.results.map(res => {
+      res.release_date = res.release_date.slice(0, 4);
+      return res;
+    });
+  
+  const markup = MovieCardTemplate(modifiedArr);
   refs.mainContainer.insertAdjacentHTML('beforeend', markup);
 }
