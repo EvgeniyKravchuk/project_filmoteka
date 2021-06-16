61936311868e10621c40c1fb97149a7c6a297ef2
EvgeniyKravchuk <gekahhh1996@gmail.com>
2021-06-16 18:05:35 +0300

    Баги

--- src/library.js
+++ src/library.js
@@ -6,10 +6,6 @@ import './js/components/localStorage';
 import './js/components/team-modal';
 import './js/components/librery-render-marcup';
 import './js/components/librery-gallery-filter';
-import { featchFilmsByIdWatched } from './js/components/librery-render-marcup';
+import {  renderQueueIfTheyActive } from './js/components/librery-render-marcup';
 
-if (localStorage.getItem('activeButton') === 'queue') {
-  return;
-} else {
-  featchFilmsByIdWatched();
-}
+renderQueueIfTheyActive()
